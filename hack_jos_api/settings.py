8be985fb11022b4d39d28398410dfafe598e6f79
GoodnessEzeokafor <gootech442@gmail.com>
2018-08-20 16:02:04 +0100

    'heroku'

--- hack_jos_api/settings.py
+++ hack_jos_api/settings.py
@@ -91,14 +91,23 @@ WSGI_APPLICATION = 'hack_jos_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default' : {
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'hack_jos',
+        'USER':'hack_jos_user',
+        'PASSWORD':'root',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
